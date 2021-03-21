71057d51eff4232e66bfe4b331aad8a2cec94899
AidenTran15 <aidenkiettran@gmail.com>
2021-03-22 02:11:40 +1100

    WIP added react-router-dom

--- crwn--clothing/src/App.js
+++ crwn--clothing/src/App.js
@@ -1,11 +1,39 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.jsx";
 
+
+const HomePage = props => {
+  return(
+    <div>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => {
+  return(
+    <div>
+      <h1>Topic List Page</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return(
+    <div>
+      <h1>Topic Detail Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/topics' component={TopicsList}/>
+      <Route path='/topics/:topicId' component={TopicDetail}/>
     </div>
   );
 }
