0b5497597673f86e34bca8903d0e653ff78dcf37
AidenTran15 <aidenkiettran@gmail.com>
2021-03-24 17:20:37 +1100

    WIP added SignIn page and Custom Button

--- crwn--clothing/src/App.js
+++ crwn--clothing/src/App.js
@@ -3,49 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 import "./App.css";
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
+import Header from './compomnent/header/header.jsx';
 
-
-// const HomePage = props => {
-//   return(
-//     <div>
-//       <button onClick={() => props.history.push('/topics')}>Topics</button>
-//       <h1>Home Page</h1>
-//     </div>
-//   );
-// };
-
-const TopicsList = () => {
-  return(
-    <div>
-      <h1>Topic List Page</h1>
-    </div>
-  );
-};
-
-const TopicDetail = () => {
-  return(
-    <div>
-      <h1>Topic Detail Page</h1>
-    </div>
-);
-};
-
-// const HatsPage = () => {
-//   return(
-//     <div>
-//       <h1>HATS PAGE</h1>
-//     </div>
-//   );
-// };
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/topics' component={TopicsList}/>
-      <Route path='/topics/:topicId' component={TopicDetail}/>
-      <Route exact path='/shop' component={ShopPage}/>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/signin' component={SignInAndSignUp}/>
+        <Route  path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
---
+++ crwn--clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-and-sign-up.scss';
+import SignIn from '../../compomnent/sign-in/sign-in';
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUp;
