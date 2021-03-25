ede3a1bf9bfeb4e52ae6e854a3482f42dd7d51cd
AidenTran15 <aidenkiettran@gmail.com>
2021-03-25 22:22:16 +1100

    added sign in sign out

--- crwn--clothing/src/App.js
+++ crwn--clothing/src/App.js
@@ -5,19 +5,43 @@ import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
 import Header from './compomnent/header/header.jsx';
+import { auth } from './firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route  path='/signin' component={SignInAndSignUp}/>
-        <Route  path='/shop' component={ShopPage}/>
-      </Switch>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuthauth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth(); 
+  }
+  
+  render () {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route  path='/signin' component={SignInAndSignUp}/>
+          <Route  path='/shop' component={ShopPage}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
