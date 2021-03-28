37637059ee25e152a8a6d9271705e3c80672073e
AidenTran15 <aidenkiettran@gmail.com>
2021-03-28 18:16:56 +1100

    Finished sign-in-and-sign-page

--- crwn--clothing/src/compomnent/sign-in/sign-in.jsx
+++ crwn--clothing/src/compomnent/sign-in/sign-in.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.scss";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -12,9 +12,16 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -46,7 +53,7 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               Sign In With Google
             </CustomButton>
           </div>
