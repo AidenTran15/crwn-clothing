e24c72b3a32cc0d6d2948c79dc6887ecce057f3e
AidenTran15 <aidenkiettran@gmail.com>
2021-03-30 16:00:23 +1100

    added redux and user action

--- crwn--clothing/src/compomnent/header/header.jsx
+++ crwn--clothing/src/compomnent/header/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assests/crown.svg";
 import "./header.scss";
@@ -29,4 +30,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps) (Header);
